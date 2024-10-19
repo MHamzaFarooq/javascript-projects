e6683b65ad3cb15a2422c17b74ea1b2e0dc2e44d
Muhammad Hamza Farooq <155229355+MHamzaFarooq@users.noreply.github.com>
2024-10-19 15:46:42 +0000

    Type Conversion Method

---
+++ basics/conversionOperation.js
@@ -0,0 +1,6 @@
+// what is type conversion?
+// when we don't know the value's datatype we use type conversion method
+
+let number = 3 + 3 // this will give us a NaN(Not a Numeber) value 
+let numberType = Boolean(number)
+console.log(numberType);
