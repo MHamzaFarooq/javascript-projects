d1ffac6a2f56b14cdaee9713f63fc892317686ad
Muhammad Hamza Farooq <155229355+MHamzaFarooq@users.noreply.github.com>
2024-10-19 15:14:36 +0000

    dataTypes information

---
+++ basics/dataTypes.js
@@ -0,0 +1,13 @@
+// Boolean => true/false
+// int => numeber
+// bigInt => very big number / rarely used
+// string => "" any word or words
+// null => standalone value
+// undefined => it's just undefined
+// symbol => unique (used in figma or react)
+// object => this is a group of multiple datatypes
+const userId = 12345
+let userName = "blud"
+
+
+console.log(typeof userName);
---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 6969
+let accountEmail = "hamza@gamil.com"
+var accountPassword = "Password12" // prefer not to use var datatype because it is obsolete
+accountCity = "Islamabad"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]) //useful to print multiple variables in a tabular form
