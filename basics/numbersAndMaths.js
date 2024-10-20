f1439f7357fc376fcf38f669a802e0471eb64146
Muhammad Hamza Farooq <155229355+MHamzaFarooq@users.noreply.github.com>
2024-10-20 17:36:37 +0000

    practice

---
+++ basics/numbersAndMaths.js
@@ -0,0 +1,17 @@
+// const balance = 100
+// console.log(balance);
+
+// const balance2 = new Number(balance)
+// console.log(balance2);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(6)); //after decimal values
+// let balance3 = 1412.263423423
+// console.log(balance3.toFixed(2));
+
+// console.log(balance3.toPrecision(5));
+
+
+let million = 1000000
+
+console.log(million.toLocaleString('en-PK'));
---
+++ basics/strings.js
@@ -0,0 +1,6 @@
+let string = "Ham za "
+
+console.log(string.split("m"));
+console.log(string.replace(" ", "-"));
+console.log(string.substring(0, 2));
+console.log(string.trimEnd());
