216b8d01af8809e13e82ea4237b2c6309fcf4271
Muhammad Hamza Farooq <155229355+MHamzaFarooq@users.noreply.github.com>
2024-10-20 09:57:33 +0000

    Revision

---
+++ basics/dataTypesSummary.js
@@ -0,0 +1,11 @@
+// two types of data types
+// 1 Primitive
+// 2 Non-Primitive or refrence type
+
+// they are categorized based on how they are stored in the memory
+
+// -------------Primitive-------------
+// String, Number, Boolean, null, undefined, bigint, symbol
+
+// -------------Non-Primitive(Reference)-------------
+// Arrays, Objects, Functions
